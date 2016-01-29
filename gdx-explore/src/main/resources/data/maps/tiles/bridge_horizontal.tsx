3d6fdce37eaa2bb77b0b6a2e1cfc2608994f9534
Lusito <core@roughael.net>
2016-01-29 23:08:52 +0100

    dummy map and entity graphics

---
+++ gdx-explore/src/main/resources/data/maps/tiles/bridge_horizontal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bridge_horizontal" tilewidth="32" tileheight="96">
+ <image source="bridge_horizontal.png" width="96" height="96"/>
+</tileset>
---
+++ gdx-explore/src/main/resources/data/maps/tiles/bridge_vertical.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bridge_vertical" tilewidth="96" tileheight="32">
+ <image source="bridge_vertical.png" width="96" height="96"/>
+</tileset>
