f1a83fd25dfa59b33c2c5c667d5045905d740113
Lusito <core@roughael.net>
2016-01-30 20:10:57 +0100

    bridge and monster fix

---
+++ gdx-explore/src/main/resources/data/maps/tiles/bridge_vertical.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bridge_vertical" tilewidth="32" tileheight="96">
+ <image source="bridge_vertical.png" width="32" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="EntityType" value="bridge_vertical"/>
+  </properties>
+ </tile>
+</tileset>
