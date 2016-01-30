ac2f488b37b0057a3cf20cfba491ee9ff88d020f
Lusito <core@roughael.net>
2016-01-30 20:44:39 +0100

    removed old scrolls

--- gdx-explore/src/main/resources/data/maps/tiles/scrolls.tsx
+++ gdx-explore/src/main/resources/data/maps/tiles/scrolls.tsx
@@ -1,19 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="scrolls" tilewidth="32" tileheight="32">
  <image source="scrolls.png" width="128" height="128"/>
- <tile id="0">
-  <properties>
-   <property name="EntityType" value="scroll_fireElemental"/>
-  </properties>
- </tile>
  <tile id="1">
   <properties>
    <property name="EntityType" value="scroll_poisonElemental"/>
-  </properties>
- </tile>
- <tile id="2">
-  <properties>
-   <property name="EntityType" value="scroll_fogElemental"/>
   </properties>
  </tile>
  <tile id="4">
@@ -24,11 +14,6 @@
  <tile id="5">
   <properties>
    <property name="EntityType" value="scroll_bomb"/>
-  </properties>
- </tile>
- <tile id="8">
-  <properties>
-   <property name="EntityType" value="scroll_bonfire"/>
   </properties>
  </tile>
  <tile id="9">
