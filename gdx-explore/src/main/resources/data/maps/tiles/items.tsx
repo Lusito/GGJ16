7ac3106b398457681a74442a2ee90a19a9c1dd45
Lusito <core@roughael.net>
2016-01-30 14:15:36 +0100

    entities for the win

--- gdx-explore/src/main/resources/data/maps/tiles/items.tsx
+++ gdx-explore/src/main/resources/data/maps/tiles/items.tsx
@@ -1,6 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="items" tilewidth="32" tileheight="32">
  <image source="items.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="EntityType" value="mushroom"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="EntityType" value="mana"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="EntityType" value="phosphor"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="EntityType" value="coal"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="EntityType" value="rope"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="EntityType" value="wood"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="EntityType" value="teleport"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="EntityType" value="stone"/>
+  </properties>
+ </tile>
  <tile id="8">
   <properties>
    <property name="EntityType" value="scroll"/>
