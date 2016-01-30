762d0bb7cac2255cbc3cc5f1f693a946007cbe92
Lusito <core@roughael.net>
2016-01-30 10:52:43 +0100

    better example for entitytype in map

--- gdx-explore/src/main/resources/data/maps/tiles/items_medium.tsx
+++ gdx-explore/src/main/resources/data/maps/tiles/items_medium.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="items_medium" tilewidth="64" tileheight="64">
  <image source="items_medium.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="EntityType" value="straw"/>
+  </properties>
+ </tile>
 </tileset>
